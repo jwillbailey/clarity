9d18b38d7ff82f768df899a13dcba9660f3f37c8
Will Bailey <78081479+wb314cam@users.noreply.github.com>
2022-05-13 09:16:58 +0100

    Update haspi_stats.py

--- analysis/HASPI_stats/haspi_stats.py
+++ analysis/HASPI_stats/haspi_stats.py
@@ -1,10 +1,34 @@
 import numpy as np
 import pandas as pd
 import json
-
+import seaborn as sns
 from scipy import stats
 from scipy.fftpack import fft
 from matplotlib import pyplot as plt
+
+
+import binsreg
+
+def binscatter(**kwargs):
+    # Estimate binsreg
+    est = binsreg.binsreg(**kwargs)
+    
+    # Retrieve estimates
+    df_est = pd.concat([d.dots for d in est.data_plot])
+    df_est = df_est.rename(columns={'x': kwargs.get("x"), 'fit': kwargs.get("y")})
+    
+    # Add confidence intervals
+    if "ci" in kwargs:
+        df_est = pd.merge(df_est, pd.concat([d.ci for d in est.data_plot]))
+        df_est = df_est.drop(columns=['x'])
+        df_est['ci'] = df_est['ci_r'] - df_est['ci_l']
+    
+    # Rename groups
+    if "by" in kwargs:
+        df_est['group'] = df_est['group'].astype(df[kwargs.get("by")].dtype)
+        df_est = df_est.rename(columns={'group': kwargs.get("by")})
+
+    return df_est
 
 metadata_path = 'F:\\clarity_CEC2_core.v1_1\\clarity_CEC2_data\\clarity_data\\metadata\\'
 exp_path = "F:\\exp\\"
@@ -34,17 +58,28 @@ for i, s in enumerate(zip(snrs['scene'], snrs['SNR'])):
 f, a = plt.subplots(2,1)
 a[0].set_title('processed audio')
 a[0].scatter(si_processed['SNR'], si_processed['haspi'], alpha = .2)
-trend = np.polyfit(si_processed['SNR'], si_processed['haspi'], 2)
+trend = np.polyfit(si_processed['SNR'], si_processed['haspi'], 4)
 trendpoly = np.poly1d(trend) 
 a[0].plot(si_processed['SNR'],trendpoly(si_processed['SNR']), color = 'red', alpha = .5)
 a[1].set_title('unprocessed audio')
 a[1].scatter(si_unprocessed['SNR'], si_unprocessed['haspi'], alpha = .2)
-trend = np.polyfit(si_unprocessed['SNR'], si_unprocessed['haspi'], 2)
+trend = np.polyfit(si_unprocessed['SNR'], si_unprocessed['haspi'], 4)
 trendpoly = np.poly1d(trend) 
 a[1].plot(si_processed['SNR'],trendpoly(si_unprocessed['SNR']), color = 'red', alpha = .5)
 plt.show()
 
 plt.scatter(si_processed['SNR'], si_processed['haspi']-si_unprocessed['haspi'])
+plt.show()
+
+f, a = plt.subplots(2,1)
+binned_si_processed = binscatter(x='SNR', y = 'haspi', data = si_processed, ci = (1,1))
+sns.scatterplot(ax = a[0], x = 'SNR', y = 'haspi', data = binned_si_processed)
+a[0].errorbar('SNR', 'haspi', yerr='ci', data=binned_si_processed, ls='', lw=2, alpha=0.2)
+a[0].set_ylim([0,1])
+binned_si_unprocessed = binscatter(x='SNR', y = 'haspi', data = si_unprocessed, ci = (1,1))
+sns.scatterplot(ax = a[1], x = 'SNR', y = 'haspi', data = binned_si_unprocessed)
+a[1].errorbar('SNR', 'haspi', yerr='ci', data=binned_si_unprocessed, ls='', lw=2, alpha=0.2)
+a[1].set_ylim([0,1])
 plt.show()
 
 plt.hist(si_processed['haspi']-si_unprocessed['haspi'], density = True)
