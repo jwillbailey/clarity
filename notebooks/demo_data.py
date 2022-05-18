1e59671812c44e3c9a12616e9da75383ba7a157b
jwillbailey <jwbailey@live.co.uk>
2022-05-18 10:07:24 +0100

    Create demo_data.py

---
+++ notebooks/demo_data.py
@@ -0,0 +1,34 @@
+import gdown
+import os
+
+
+def get_targets_demo():
+    targets_url = "https://drive.google.com/uc?export=download&id=1uu2Hes1fzqNaZSCiFNhxZM3bE_fAVKsD"
+    output = "targets.tgz"
+    gdown.download(targets_url, output, quiet=False)
+    os.system("tar -xzf targets.tgz")
+    os.system("rm targets.tgz")
+
+
+def get_interferers_demo():
+    interferers_url = "https://drive.google.com/uc?export=download&id=1_ssD238Qv-EETzC0hJze7JhLE7bHyqwG"
+    output = "interferers.tgz"
+    gdown.download(interferers_url, output, quiet=False)
+    os.system("tar -xzf interferers.tgz")
+    os.system("rm interferers.tgz")
+
+
+def get_rooms_demo():
+    rooms_url = "https://drive.google.com/uc?export=download&id=1FBC8DI4Ru-g3Set0fDzoKmXTqHqNXV8n"
+    output = "rooms.tgz"
+    gdown.download(rooms_url, output, quiet=False)
+    os.system("tar -xzf rooms.tgz")
+    os.system("rm rooms.tgz")
+
+
+def get_scenes_demo():
+    scenes_url = "https://drive.google.com/uc?export=download&id=1PB0CfGXhpkYNk8HbE5lTWowm2016x6Hl"
+    output = "scenes.tgz"
+    gdown.download(scenes_url, output, quiet=False)
+    os.system("tar -xzf scenes.tgz")
+    os.system("rm scenes.tgz")
